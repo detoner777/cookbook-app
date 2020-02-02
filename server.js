be170a9544fe769a22c772a4e7bea02c05605e61
Alexey_Lisenko <detoner777@gmail.com>
2020-02-02 17:16:47 +0200

    DB config

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://detoner:detoner@cookbook-app-cluster-xsykg.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(uri, {
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri || "mongodb://localhost/my_database", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
